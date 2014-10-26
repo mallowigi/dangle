9b7182f5aad2ce3c13919408ed0d19fb83e85168
eliorb <eliorb@codeoasis.com>
2014-10-26 17:19:42 +0200

    Separating the module to take only the donut part, and add bower

--- grunt.js
+++ grunt.js
@@ -15,93 +15,15 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<banner:meta.banner>',
-          'src/dangle.module.js',
           'src/modules/**/*.js'
         ],
-        dest: 'dist/dangle.js'
-      },
-      dangle: {
-        src: [
-          '<banner:meta.banner>',
-          'src/dangle.module.js'
-        ],
-        dest: 'dist/dangle.module.js'
-      },
-      donut: {
-        src: [
-          '<banner:meta.banner>',
-          'src/modules/donut/donut.js'
-        ],
         dest: 'dist/dangle.donut.js'
-      },
-      pie: {
-        src: [
-          '<banner:meta.banner>',
-          'srcmodules/pie/pie.js'
-        ],
-        dest: 'dist/dangle.pie.js'
-      },
-      bar: {
-        src: [
-          '<banner:meta.banner>',
-          'src/modules/bar/bar.js'
-        ],
-        dest: 'dist/dangle.bar.js'
-      },
-      column: {
-        src: [
-          '<banner:meta.banner>',
-          'src/modules/column/column.js'
-        ],
-        dest: 'dist/dangle.column.js'
-      },
-      area: {
-        src: [
-          '<banner:meta.banner>',
-          'src/modules/area/area.js'
-        ],
-        dest: 'dist/dangle.area.js'
-      },
-      datehisto: {
-        src: [
-          '<banner:meta.banner>',
-          'src/modules/date-histo/datehisto.js'
-        ],
-        dest: 'dist/dangle.datehisto.js'
       }
     },
     min: {
       dist: {
         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/dangle.min.js'
-      },
-      module: {
-        src: ['<banner:meta.banner>', 'src/dangle.module.js'],
-        dest: 'dist/dangle.module.min.js'
-      },
-      donut: {
-        src: ['<banner:meta.banner>', 'src/modules/donut/donut.js'],
         dest: 'dist/dangle.donut.min.js'
-      },
-      pie: {
-        src: ['<banner:meta.banner>', 'src/modules/pie/pie.js'],
-        dest: 'dist/dangle.pie.min.js'
-      },
-      bar: {
-        src: ['<banner:meta.banner>', 'src/modules/bar/bar.js'],
-        dest: 'dist/dangle.bar.min.js'
-      },
-      column: {
-        src: ['<banner:meta.banner>', 'src/modules/column/column.js'],
-        dest: 'dist/dangle.column.min.js'
-      },
-      area: {
-        src: ['<banner:meta.banner>', 'src/modules/area/area.js'],
-        dest: 'dist/dangle.area.min.js'
-      },
-      datehisto: {
-        src: ['<banner:meta.banner>', 'src/modules/date-histo/datehisto.js'],
-        dest: 'dist/dangle.datehisto.min.js'
       }
     },
     lint: {
